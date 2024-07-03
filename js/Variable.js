8ff6d2305639ff39e76af21d42580ec1065fd3f1
HARRYmuirHDZ <pottermuir@gmail.com>
2024-07-02 21:25:50 -0600

    feat-tipoDeDatos

--- js/Variable.js
+++ js/Variable.js
@@ -23,17 +23,29 @@ console.log(nombre)
 
 
 
-let Nombre ="Harry"
-let Apellido ="Muir"
-let Edad =27
-let Direccion ="25 metros sureste en mi casa"
+let Nombre = "Harry" //TEXTO -String O CADENA
+let Apellido = "Muir"
+let Edad = 27  //NUMERICOS, INTEGER, DECIMAL SIN COMILLAS DOBLES
+let Direccion = "25 metros sureste en mi casa"
 
+let Mensaje =
+    " Hola mi nombre es " + Nombre +
+    " Y mi apellido es " + Apellido +
+    " mi edad es " + Edad +
+    " vivo en " + Direccion
 
-console.log(nombre)
-console.log(apellido)
-console.log(edad)
-console.log(direccion)
+let Mensaje2 =  "Es usted mayor de edad ? "
+let Ventana 
+// console.log(nombre)
+// console.log(apellido)
+// console.log(edad)
+// console.log(direccion)
 
-console.log("Hola mi nombre es :"+Nombre+" Y mi apellido es "+" mi edad es " +Edad+"vivo en " +Direccion)
+console.log(Mensaje)
 
+// capturar lo que digito el usuario
+Ventana = prompt(Mensaje2)
+
+// mostrar un mensaje en una Ventana emergente 
+alert(Ventana)
 
