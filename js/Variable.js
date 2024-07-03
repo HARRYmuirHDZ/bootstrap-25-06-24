c58f3afe53abf95cccc427401b1287c6889885ff
HARRYmuirHDZ <pottermuir@gmail.com>
2024-07-02 20:01:56 -0600

    feat-practica de JS

---
+++ js/Variable.js
@@ -0,0 +1,37 @@
+// Lenguaje de programacion
+
+// Mostrar un mensaje en la consola  del navegador
+
+// .btn{
+//     color:white
+// }
+
+// function btn() {
+
+// }
+
+
+console.log("Hola Mundo")
+
+// 1. variable almacenan datos var = let    las variables cambian por ejemplo cuando metemos la edad ejm: let edad= si no ponemos el igual no se inicializa let edad= 29
+// const PI=3,14 es el principio de la variable, Nunca cambia
+
+
+let nombre = "Hola Mundo"
+
+console.log(nombre)
+
+
+
+let Nombre ="Harry"
+let apellido ="Muir"
+let edad =27
+let direccion ="25 metros sureste en mi casa"
+
+
+console.log(nombre)
+console.log(apellido)
+console.log(edad)
+console.log(direccion)
+
+
