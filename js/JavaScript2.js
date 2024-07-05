8615f3ee966be2a11b1b94f869e906658074a93c
HARRYmuirHDZ <pottermuir@gmail.com>
2024-07-04 19:26:49 -0600

    FEAT-DECLARACION

---
+++ js/JavaScript2.js
@@ -0,0 +1,34 @@
+let paises = ["CR","USA","PA"]
+//3 ELEMENTOS EN EL ARRAY
+
+
+//inician de en la posicion o
+
+console.log(paises[1])
+console.log(paises[2])
+
+let estudiante ={
+    nombre: "pedro",
+    edad: 27,
+    activo:true,
+    materias: ["progra1","base de datos","calculo"]
+
+}
+
+
+console.log(estudiante)//muestra todo
+console.log(estudiante.nombre)
+console.log(estudiante.materias[2])
+
+// Declaren el siguiente Objeto Auto:  4 propiedades string,int,boolean y array
+
+
+let Auto ={
+    marca: "lambo",
+    llantas: 4,
+    activo:true,
+    velocidad:[20,60,80,120]
+
+}
+
+console.log(Auto.velocidad[20])
