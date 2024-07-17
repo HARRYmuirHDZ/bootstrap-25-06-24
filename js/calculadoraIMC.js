f39aa0499c24606615382d3c18303469205cd4ae
HARRYmuirHDZ <pottermuir@gmail.com>
2024-07-16 19:02:23 -0600

    feat-Nuevo Repo

--- js/calculadoraIMC.js
+++
@@ -1,6 +0,0 @@
-function calcularIMC(event) {
-    event.preventDefault();
-
-    const peso = parseFloat(document.getElementById('peso').value);
-    const altura = parseFloat(document.getElementById('altura').value);
-    const resultado = document.getElementById('resultado');
